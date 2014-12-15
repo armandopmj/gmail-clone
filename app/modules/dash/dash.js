86a0ec8f88a2ce2256ddbcf99f8b840c0c0c6058
Armando Perez <armandopmj@gmail.com>
2014-12-14 21:46:53 -0800

    (feat) Add IIFE and 'use strict' to angular files

--- app/modules/dash/dash.js
+++ app/modules/dash/dash.js
@@ -1,47 +1,51 @@
-angular
-.module('gmailApp.dash', [])
-.config([ '$stateProvider', function( $stateProvider ){
-    $stateProvider
-        .state( 'dash', {
-            url: '/dash',
-            templateUrl: 'modules/dash/dash.html',
-            controller: 'dashController'
-        })
-}])
-.factory('dashService', [ 
-    '$http', 
-    dashService
-])
-.controller('dashController', [ 
-    '$scope', 
-    'dashService', 
-    dashController
-]);
-function dashService( $http ) {
-    //TODO: Make api call to retrive user emails
-    var obj = {}
-    obj.getInbox = getInbox;
-    return obj;
+(function(){
+    //TODO: reaserch 'use strict' in IIFE
+    'use strict';
+    
+    angular
+    .module('gmailApp.dash', [])
+    .config([ '$stateProvider', function( $stateProvider ){
+        $stateProvider
+            .state( 'dash', {
+                url: '/dash',
+                templateUrl: 'modules/dash/dash.html',
+                controller: 'dashController'
+            })
+    }])
+    .factory('dashService', [ 
+        '$http', 
+        dashService
+    ])
+    .controller('dashController', [ 
+        '$scope', 
+        'dashService', 
+        dashController
+    ]);
+    function dashService( $http ) {
+        //TODO: Make api call to retrive user emails
+        var obj = {}
+        obj.getInbox = getInbox;
+        return obj;
 
-    function getInbox( userName ){ 
-        return $http({ 
-                url: 'api/inbox/' + userName, 
-                method: 'GET'
-                // params: {'userName': 'Armando Perez'}
-            })
-            .then( function( data ){
-                console.log('inside factory:', data.data );
-                return data;
-            })
+        function getInbox( userName ){ 
+            //TODO: Refactor request to resolve, then retrun value
+            //      vs returning promise 
+
+            return $http({ 
+                    url: 'api/inbox/' + userName, 
+                    method: 'GET'
+                    // params: {'userName': 'Armando Perez'}
+                })
+        }
     }
-}
-function dashController( $scope, dashService ){
-    $scope.boxes = ['inbox', 'sent', 'trash', 'spam'];
-        var messages;
-        
-        dashService.getInbox( 'Armando Perez' )
-            .then(function( result ){
-                messages = result;
-                console.log( 'messages', messages );
-            });
-}
+    function dashController( $scope, dashService ){
+        $scope.boxes = ['inbox', 'sent', 'trash', 'spam'];
+            var messages;
+            
+            dashService.getInbox( 'Armando Perez' )
+                .then(function( result ){
+                    messages = result;
+                    console.log( 'messages', messages );
+                });
+    }
+})();
--- app/modules/login/login.js
+++ app/modules/login/login.js
@@ -1,45 +1,50 @@
-angular
-.module('gmailApp.login', [])
-.config(['$stateProvider', function($stateProvider) {
-    $stateProvider.state(
-        'login', {
-            url: '/login',
-            templateUrl: 'modules/login/login.html',
-            // controllerAs: 'login',
-            controller: 'LoginCtrl'
-        }
-    );
-}])
-.factory('LoginService', [
-    LoginService
-])  
-.controller('LoginCtrl', [
-    '$scope',
-    '$state',
-    'LoginService',
-    'LoggedInUser',
-    LoginCtrl
-]);
+(function(){
+    //TODO: reaserch 'use strict' in IIFE
+    'use strict';
 
-function LoginCtrl($scope, $state, LoginService, LoggedInUser){
-    $scope.userLogin = function(){ 
-        var data = {
-            name: $scope.name,
-            password: $scope.password
+    angular
+    .module('gmailApp.login', [])
+    .config(['$stateProvider', function($stateProvider) {
+        $stateProvider.state(
+            'login', {
+                url: '/login',
+                templateUrl: 'modules/login/login.html',
+                // controllerAs: 'login',
+                controller: 'LoginCtrl'
+            }
+        );
+    }])
+    .factory('LoginService', [
+        LoginService
+    ])  
+    .controller('LoginCtrl', [
+        '$scope',
+        '$state',
+        'LoginService',
+        'LoggedInUser',
+        LoginCtrl
+    ]);
+
+    function LoginCtrl($scope, $state, LoginService, LoggedInUser){
+        $scope.userLogin = function(){ 
+            var data = {
+                name: $scope.name,
+                password: $scope.password
+            };
+            LoginService.login(data, function(){
+                //TODO: refactor this logic out of anonymous function and into service
+                console.log( data );
+                LoggedInUser.name = data.name;
+                $state.go( 'dash' );
+            });   
         };
-        LoginService.login(data, function(){
-            //TODO: refactor this logic out of anonymous function and into service
-            console.log( data );
-            LoggedInUser.name = data.name;
-            $state.go( 'dash' );
-        });   
     };
-};
 
-function LoginService(){
-    loginServ = {};
-    loginServ.login = function( data, callback ) {
-        callback();
+    function LoginService(){
+        loginServ = {};
+        loginServ.login = function( data, callback ) {
+            callback();
+        }
+        return loginServ;
     }
-    return loginServ;
-}
+})();
